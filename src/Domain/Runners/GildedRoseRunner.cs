dfe5fa3ac5425af70b3eec0f8bb8cc618c27dc8d
Ryan Robinson <ryan.robinson@ao.com>
2023-12-05 12:01:51 +0000

    chore: pull out a builder

--- src/Domain/Runners/GildedRoseRunner.cs
+++ src/Domain/Runners/GildedRoseRunner.cs
@@ -2,16 +2,46 @@
 {
     public class GildedRoseRunner
     {
+        public GildedRoseRunner()
+        {
+            
+        }
+
         public void Run()
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+            var Items = InitialSetup();
+
+            var app = new GildedRose(Items);
+
+            for (var i = 0; i < 31; i++)
+            {
+                Console.WriteLine("-------- day " + i + " --------");
+                Console.WriteLine("name, sellIn, quality");
+                for (var j = 0; j < Items.Count; j++)
+                {
+                    Console.WriteLine(Items[j]);
+                }
+
+                if (i < 30)
+                {
+                    Console.WriteLine("");
+                }
+
+                app.UpdateQuality();
+            }
+        }
+
+        private static IList<Item> InitialSetup()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 },
                 new Item
                 {
                     Name = "Backstage passes to a TAFKAL80ETC concert",
@@ -32,27 +62,9 @@
                 },
 
                 // this conjured item does not work properly yet
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
             };
-
-            var app = new GildedRose(Items);
-
-            for (var i = 0; i < 31; i++)
-            {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    Console.WriteLine(Items[j]);
-                }
-
-                if (i < 30)
-                {
-                    Console.WriteLine("");
-                }
-
-                app.UpdateQuality();
-            }
+            return Items;
         }
     }
 }
